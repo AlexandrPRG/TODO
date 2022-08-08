48e5555251f771fc21d00201257536c96731bc97
AlexandrPRG <irdsbk@bk.ru>
2022-08-08 09:50:00 +0300

    step 4 worked

---
+++ TODOnotes/users/models.py
@@ -0,0 +1,8 @@
+from django.db import models
+from django.contrib.auth.models import AbstractUser
+
+class User(models.Model):
+    username = models.CharField(max_length=64)
+    first_name = models.CharField(max_length=64)
+    last_name = models.CharField(max_length=64)
+    email = models.CharField(max_length=64, unique=True)
