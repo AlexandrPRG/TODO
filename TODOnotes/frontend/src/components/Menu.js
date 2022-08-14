a1c61de24e649902072cb926cb9481ef2999ae5f
AlexandrPRG <irdsbk@bk.ru>
2022-08-14 18:30:26 +0300

    step menu

--- TODOnotes/frontend/src/components/Menu.js
+++ TODOnotes/frontend/src/components/Menu.js
@@ -4,8 +4,13 @@ import React from "react";
 const Menu = (menu) => {
     return (
     <div>
-        <ul>
-            <li><a href="#">{menu.menuitem[0]}</a></li>
+        <ul
+        style="
+          margin: 0;
+          padding: 0;
+          list-style-type: none;
+         ">
+            <li ><a href="#">{menu.menuitem[0]}</a></li>
             <li><a href="#">{menu.menuitem[1]}</a></li>
             <li><a href="#">{menu.menuitem[2]}</a></li>
         </ul>
