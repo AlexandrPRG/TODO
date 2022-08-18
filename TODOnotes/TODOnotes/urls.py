17f073e93207a9c23eecc44a80ede15da10abcd0
AlexandrPRG <irdsbk@bk.ru>
2022-08-18 10:12:04 +0300

    step 0

--- TODOnotes/TODOnotes/urls.py
+++ TODOnotes/TODOnotes/urls.py
@@ -18,9 +18,12 @@ from django.urls import path, include
 from rest_framework.routers import DefaultRouter
 
 from users.views import UserModelViewSet
+from notes.views import ProjectModelViewSet,TodoModelViewSet
 
 router = DefaultRouter()
 router.register('users', UserModelViewSet)
+router.register('projects', ProjectModelViewSet)
+router.register('todoes', TodoModelViewSet)
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api-auth/', include('rest_framework.urls')),
